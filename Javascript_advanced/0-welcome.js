2ab90c944554007f2f8c30aff295bcaf469b744c
Beverly Mugwadi <b.mugwadi1@alustudent.com>
2025-06-29 21:11:42 +0200

    All files

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,10 @@
+const welcome = (firstName, lastName) => {
+    const fullName = `${firstName} ${lastName}`;
+  
+    const displayFullName = () => {
+      alert(`Welcome ${fullName}!`);
+    };
+    displayFullName();
+  };
+
+  
